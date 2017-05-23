e87a4e70cfd864d0013cb402c4e5250aac521eea
soif <wxopwx@gmail.com>
2017-05-23 13:28:08 +0200

    Add license comment

--- pcag.php
+++ pcag.php
@@ -1,16 +1,26 @@
 #!/usr/bin/php -q
 <?php
 /*
-	Inspired by	http://kvz.io/blog/2009/01/09/create-daemons-in-php/
-	--------------------------------------------------------------------------------------
+	PhpCameraAlarmGateway
+	https://github.com/soif/PhpCameraAlarmGateway
+	Copyright (C) 2017  Francois Dechery
 
-	PEAR Must be installed for this to work! 
-	Under Debian:
-		apt-get install php-pear
-		pear install System_Daemon 
-		pear install Log
+	LICENCE: ###########################################################
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+	#####################################################################
 */
+
 
 // Make it possible to test in  source directory This is for PEAR developers only
 //ini_set('include_path',  ini_get('include_path').':..');
